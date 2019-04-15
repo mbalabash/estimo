07e76b6f13b8bdc4a746a88440b546e733aa72c2
Maksim Balabash <maksim.balabash@gmail.com>
2019-04-15 17:04:24 +0300

    Add tests for utils

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,14 @@
-const path = require('path')
+const fs = require('fs')
 const test = require('ava')
-const { resolvePathToTempDir, getUrlToHtmlFile } = require('../src/utils')
+const path = require('path')
+const nanoid = require('nanoid')
+const {
+  resolvePathToTempDir,
+  getUrlToHtmlFile,
+  deleteFile,
+  writeFile,
+  readFile,
+} = require('../src/utils')
 
 test('should correctly resolve path to file in temp directory', (t) => {
   const fileName = 'someFile.txt'
@@ -17,4 +25,59 @@ test('should correctly generate url to file', (t) => {
     getUrlToHtmlFile(resolvePathToTempDir(fileName)),
     `file://${path.resolve(path.join(__dirname, '../temp/', fileName))}`,
   )
+})
+
+test('should write file in right place', async (t) => {
+  const customTempDir = '../test/__mock__/'
+  const fileName = `${nanoid()}.txt`
+  const fileContent = 'information'
+  const filePath = resolvePathToTempDir(fileName, customTempDir)
+
+  let isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, false)
+
+  await writeFile(filePath, fileContent)
+  isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, true)
+
+  fs.unlinkSync(filePath)
+})
+
+test('should delete file in right place', async (t) => {
+  const customTempDir = '../test/__mock__/'
+  const fileName = `${nanoid()}.txt`
+  const fileContent = 'information'
+  const filePath = resolvePathToTempDir(fileName, customTempDir)
+
+  let isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, false)
+
+  await writeFile(filePath, fileContent)
+  isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, true)
+
+  await deleteFile(filePath)
+  isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, false)
+})
+
+test('should correctly read data from file', async (t) => {
+  const customTempDir = '../test/__mock__/'
+  const fileName = `${nanoid()}.txt`
+  const fileContent = 'information'
+  const filePath = resolvePathToTempDir(fileName, customTempDir)
+
+  let isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, false)
+
+  await writeFile(filePath, fileContent)
+  isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, true)
+
+  const content = await readFile(filePath)
+  t.is(content, 'information')
+
+  await deleteFile(filePath)
+  isFileExist = fs.existsSync(filePath)
+  t.is(isFileExist, false)
 })
