eedc416ad4a5b387e8a45e8978bfcb8373a59073
mbalabash <maksim.balabash@gmail.com>
2019-05-02 17:23:51 +0300

    Move examples to own project

--- example/simple.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const estimo = require('../index')
-
-;
-
-(async () => {
-  const report = await estimo(path.resolve(path.join(__dirname, './lib/2kb.js')))
-  console.log(report)
-})()
