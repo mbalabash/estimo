26ca34f106c3d1bb5e6fead71ecbb7cd891df582
Maksim Balabash <maksim.balabash@gmail.com>
2019-06-20 00:53:45 +0300

    Play with chrome test, trying to fix 10 min timeout

--- test/reporter.test.js
+++ test/reporter.test.js
@@ -1,6 +1,6 @@
 const test = require('ava')
 const path = require('path')
-const { formatTime, getEventsTime } = require('../src/reporter')
+const { generateReadableReport, formatTime, getEventsTime } = require('../src/reporter')
 const { createChromeTrace } = require('../src/createChromeTrace')
 const { generateHtmlFiles } = require('../src/generateHtmlFiles')
 const { removeTempFiles } = require('../src/utils')
@@ -10,52 +10,51 @@ test('should create valid report for one lib', async t => {
 
   const htmlFiles = await generateHtmlFiles([lib1])
   const traceFiles = await createChromeTrace(htmlFiles, {})
-  // const report = await generateReadableReport(traceFiles)
+  const report = await generateReadableReport(traceFiles)
 
-  // const { library, total, javaScript, parseHTML } = report[0]
-  // t.is(library, '13kb.js')
-  t.is(typeof traceFiles === 'object', true)
-  // t.is(typeof total === 'number' && total > 0, true)
-  // t.is(typeof javaScript === 'number' && javaScript > 0, true)
-  // t.is(typeof parseHTML === 'number' && parseHTML > 0, true)
+  const { library, total, javaScript, parseHTML } = report[0]
+  t.is(library, '13kb.js')
+  t.is(typeof total === 'number' && total > 0, true)
+  t.is(typeof javaScript === 'number' && javaScript > 0, true)
+  t.is(typeof parseHTML === 'number' && parseHTML > 0, true)
 
   await removeTempFiles(htmlFiles.map(file => file.html))
-  // await removeTempFiles(traceFiles.map(file => file.traceFile))
+  await removeTempFiles(traceFiles.map(file => file.traceFile))
 })
 
-// test('should create valid report for many libs', async (t) => {
-//   const lib1 = path.resolve(path.join(__dirname, '__mock__', '19kb.js'))
-//   const lib2 = path.resolve(path.join(__dirname, '__mock__', '13kb.js'))
+test('should create valid report for many libs', async t => {
+  const lib1 = path.resolve(path.join(__dirname, '__mock__', '19kb.js'))
+  const lib2 = path.resolve(path.join(__dirname, '__mock__', '13kb.js'))
 
-//   const htmlFiles = await generateHtmlFiles([lib1, lib2])
-//   const traceFiles = await createChromeTrace(htmlFiles, {})
-//   const report = await generateReadableReport(traceFiles)
+  const htmlFiles = await generateHtmlFiles([lib1, lib2])
+  const traceFiles = await createChromeTrace(htmlFiles, {})
+  const report = await generateReadableReport(traceFiles)
 
-//   const {
-//     library: library1,
-//     total: total1,
-//     javaScript: javaScript1,
-//     parseHTML: parseHTML1,
-//   } = report[0]
-//   t.is(library1, '19kb.js')
-//   t.is(typeof total1 === 'number' && total1 > 0, true)
-//   t.is(typeof javaScript1 === 'number' && javaScript1 > 0, true)
-//   t.is(typeof parseHTML1 === 'number' && parseHTML1 > 0, true)
+  const {
+    library: library1,
+    total: total1,
+    javaScript: javaScript1,
+    parseHTML: parseHTML1,
+  } = report[0]
+  t.is(library1, '19kb.js')
+  t.is(typeof total1 === 'number' && total1 > 0, true)
+  t.is(typeof javaScript1 === 'number' && javaScript1 > 0, true)
+  t.is(typeof parseHTML1 === 'number' && parseHTML1 > 0, true)
 
-//   const {
-//     library: library2,
-//     total: total2,
-//     javaScript: javaScript2,
-//     parseHTML: parseHTML2,
-//   } = report[1]
-//   t.is(library2, '13kb.js')
-//   t.is(typeof total2 === 'number' && total2 > 0, true)
-//   t.is(typeof javaScript2 === 'number' && javaScript2 > 0, true)
-//   t.is(typeof parseHTML2 === 'number' && parseHTML2 > 0, true)
+  const {
+    library: library2,
+    total: total2,
+    javaScript: javaScript2,
+    parseHTML: parseHTML2,
+  } = report[1]
+  t.is(library2, '13kb.js')
+  t.is(typeof total2 === 'number' && total2 > 0, true)
+  t.is(typeof javaScript2 === 'number' && javaScript2 > 0, true)
+  t.is(typeof parseHTML2 === 'number' && parseHTML2 > 0, true)
 
-//   await removeTempFiles(htmlFiles.map(file => file.html))
-//   await removeTempFiles(traceFiles.map(file => file.traceFile))
-// })
+  await removeTempFiles(htmlFiles.map(file => file.html))
+  await removeTempFiles(traceFiles.map(file => file.traceFile))
+})
 
 test('should correctly format time', t => {
   t.is(formatTime(11.2223123131231), 11.22)
