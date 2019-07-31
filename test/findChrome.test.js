5f4d8c3a9ebbbba19f040f66b1c198be5fd836d9
Maksim Balabash <maksim.balabash@gmail.com>
2019-07-31 20:10:08 +0300

    Remove unused test

--- test/findChrome.test.js
+++ test/findChrome.test.js
@@ -3,21 +3,9 @@ const path = require('path')
 const { readFile, writeFile } = require('../src/utils')
 const { findChrome } = require('../scripts/chromeDetection')
 
-async function readChromeConfig() {
-  return JSON.parse(await readFile(path.join(__dirname, '..', 'chrome.json')))
-}
-
-test.afterEach(async () => {
-  await writeFile(path.join(__dirname, '..', 'chrome.json'), '{ "executablePath": "" }')
-})
-
-test.serial('chrome.json should contains correct empty value', async t => {
-  const emptyConfigData = await readChromeConfig()
-  t.deepEqual(emptyConfigData, { executablePath: '' })
-})
-
-test.serial('should set location setting for downloaded or local chrome', async t => {
+test('should set location setting for downloaded or local chrome', async t => {
   await findChrome()
-  const configData = await readChromeConfig()
+  const configData = JSON.parse(await readFile(path.join(__dirname, '..', 'chrome.json')))
   t.is(configData.executablePath.includes('chrome'), true)
+  await writeFile(path.join(__dirname, '..', 'chrome.json'), '{ "executablePath": "" }')
 })
