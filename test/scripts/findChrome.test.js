ac478f4ce2fcccce9801d7fb3092f7c8a40d63ad
Maksim Balabash <maksim.balabash@gmail.com>
2019-08-27 23:29:39 +0300

    Add test findChrome

--- test/scripts/findChrome.test.js
+++ test/scripts/findChrome.test.js
@@ -4,8 +4,11 @@ const { readFile, writeFile } = require('../../src/utils')
 const { findChrome } = require('../../scripts/chromeDetection')
 
 test('should set location setting for downloaded or local chrome', async t => {
-  await findChrome()
-  const configData = JSON.parse(await readFile(path.join(__dirname, '..', 'chrome.json')))
-  t.is(configData.executablePath.includes('chrome'), true)
-  await writeFile(path.join(__dirname, '..', 'chrome.json'), '{ "executablePath": "" }')
+  const pathToChrome = await findChrome()
+  const configData = JSON.parse(await readFile(path.join(__dirname, '../..', 'chrome.json')))
+
+  t.is(typeof pathToChrome === 'string' && pathToChrome.length > 0, true)
+  t.is(configData.executablePath.length > 0, true)
+
+  await writeFile(path.join(__dirname, '../..', 'chrome.json'), '{ "executablePath": "" }')
 })
