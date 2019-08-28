7ab5b6bd2d99b072e851ece2dbb79bb3c43c9e53
Maksim Balabash <maksim.balabash@gmail.com>
2019-08-28 10:38:04 +0300

    Update page-mode test

--- test/page-mode/page-mode.test.js
+++ test/page-mode/page-mode.test.js
@@ -8,7 +8,7 @@ test('estimoPageMode - should works properly', async (t) => {
   const chromeLocation = await findChrome()
 
   const page1 = 'https://www.google.com/'
-  const page2 = 'https://developers.google.com/web/tools/puppeteer/'
+  const page2 = 'https://translate.google.com/'
 
   const reports = await estimoPageMode([page1, page2], { executablePath: chromeLocation })
 
@@ -17,7 +17,7 @@ test('estimoPageMode - should works properly', async (t) => {
   t.is(typeof reports[1].javaScript === 'number' && reports[1].javaScript > 0, true)
   t.is(typeof reports[1].parseHTML === 'number' && reports[1].parseHTML > 0, true)
 
-  t.is(reports[1].name, 'https://developers.google.com/web/tools/puppeteer/')
+  t.is(reports[1].name, 'https://translate.google.com/')
   t.is(typeof reports[1].total === 'number' && reports[1].total > 0, true)
   t.is(typeof reports[1].javaScript === 'number' && reports[1].javaScript > 0, true)
   t.is(typeof reports[1].parseHTML === 'number' && reports[1].parseHTML > 0, true)
