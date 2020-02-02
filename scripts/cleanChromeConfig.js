5fdf65df94054a5cba2022358647000a7e4e1534
Maksim Balabash <maksim.balabash@gmail.com>
2020-02-02 11:43:06 +0300

    improvement: Auto clean chrome.json

---
+++ scripts/cleanChromeConfig.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const { writeFile } = require('../src/utils')
+
+async function cleanChromeConfig() {
+  await writeFile(path.join(__dirname, '..', 'chrome.json'), '{ "executablePath": "" }')
+}
+
+cleanChromeConfig()
