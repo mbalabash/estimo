c212be312d301d6832ec54021c0f15005bb396fd
Maksim Balabash <maksim.balabash@gmail.com>
2020-07-12 23:04:42 +0300

    chore: Update test-launch script.

--- tests/__mock__/test-launch.js
+++ tests/__mock__/test-launch.js
@@ -6,6 +6,8 @@ console.log(__dirname)
 const localJsFile = path.join(__dirname, '19kb.js')
 
 ;(async () => {
-  const reports = await estimo([localJsFile])
+  // const reports = await estimo(['https://news.ycombinator.com/news', 'https://www.google.com/'], {runs: 13})
+  const reports = await estimo([localJsFile, 'https://github.githubassets.com/assets/compat-bootstrap-6e7ff7ac.js'], {runs: 13})
   console.log(reports)
 })()
+// ESTIMO_DEBUG=true 
