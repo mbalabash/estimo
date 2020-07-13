e1a07bfa98b2c610c52c83a396823495c1d73ad3
Maksim Balabash <maksim.balabash@gmail.com>
2020-07-13 16:45:23 +0300

    test: Add tests for runs api.

--- tests/js-mode/js-mode.test.js
+++ tests/js-mode/js-mode.test.js
@@ -29,3 +29,30 @@ test('estimoJsMode - should works properly', async (t) => {
 
   await writeFile(path.join(__dirname, '../..', 'chrome.json'), '{ "executablePath": "" }')
 })
+
+test('estimoJsMode with RUNS option - should works properly', async (t) => {
+  const chromeLocation = await findChrome()
+
+  const lib1 = path.join(__dirname, '..', '__mock__', '19kb.js')
+  const reports = await estimoJsMode([lib1], { executablePath: chromeLocation, runs: 13 })
+
+  t.is(reports.length, 1)
+  t.is(reports[0].name, '19kb.js')
+  t.is(typeof reports[0].parseHTML === 'number' && reports[0].parseHTML >= 0, true)
+  t.is(typeof reports[0].styleLayout === 'number' && reports[0].styleLayout >= 0, true)
+  t.is(
+    typeof reports[0].paintCompositeRender === 'number' && reports[0].paintCompositeRender >= 0,
+    true
+  )
+  t.is(
+    typeof reports[0].scriptParseCompile === 'number' && reports[0].scriptParseCompile >= 0,
+    true
+  )
+  t.is(typeof reports[0].scriptEvaluation === 'number' && reports[0].scriptEvaluation >= 0, true)
+  t.is(typeof reports[0].javaScript === 'number' && reports[0].javaScript > 0, true)
+  t.is(typeof reports[0].garbageCollection === 'number' && reports[0].garbageCollection >= 0, true)
+  t.is(typeof reports[0].other === 'number' && reports[0].other >= 0, true)
+  t.is(typeof reports[0].total === 'number' && reports[0].total > 0, true)
+
+  await writeFile(path.join(__dirname, '../..', 'chrome.json'), '{ "executablePath": "" }')
+})
--- tests/page-mode/page-mode.test.js
+++ tests/page-mode/page-mode.test.js
@@ -29,3 +29,30 @@ test('estimoPageMode - should works properly', async (t) => {
 
   await writeFile(path.join(__dirname, '../..', 'chrome.json'), '{ "executablePath": "" }')
 })
+
+test('estimoPageMode with RUNS option - should works properly', async (t) => {
+  const chromeLocation = await findChrome()
+
+  const page = getUrlToHtmlFile(path.join(__dirname, '..', '__mock__', 'test.html'))
+  const reports = await estimoPageMode([page], { executablePath: chromeLocation, runs: 13 })
+
+  t.is(reports.length, 1)
+  t.is(reports[0].name, page)
+  t.is(typeof reports[0].parseHTML === 'number' && reports[0].parseHTML >= 0, true)
+  t.is(typeof reports[0].styleLayout === 'number' && reports[0].styleLayout >= 0, true)
+  t.is(
+    typeof reports[0].paintCompositeRender === 'number' && reports[0].paintCompositeRender >= 0,
+    true
+  )
+  t.is(
+    typeof reports[0].scriptParseCompile === 'number' && reports[0].scriptParseCompile >= 0,
+    true
+  )
+  t.is(typeof reports[0].scriptEvaluation === 'number' && reports[0].scriptEvaluation >= 0, true)
+  t.is(typeof reports[0].javaScript === 'number' && reports[0].javaScript > 0, true)
+  t.is(typeof reports[0].garbageCollection === 'number' && reports[0].garbageCollection >= 0, true)
+  t.is(typeof reports[0].other === 'number' && reports[0].other >= 0, true)
+  t.is(typeof reports[0].total === 'number' && reports[0].total > 0, true)
+
+  await writeFile(path.join(__dirname, '../..', 'chrome.json'), '{ "executablePath": "" }')
+})
