6db67a468fdb309bd2e7a7a798e5d509adeec170
Maksim Balabash <maksim.balabash@gmail.com>
2021-11-16 23:19:50 +0300

    Update BrowserOptions type

--- src/@types/browserOptions.ts
+++ src/@types/browserOptions.ts
@@ -4,15 +4,8 @@ export interface BrowserOptions {
   userAgent?: string
   device?: string
 
-  emulateNetworkConditions?: boolean
-  offline?: boolean
-  latency?: number
-  downloadThroughput?: number
-  uploadThroughput?: number
-  connectionType?: string
-
-  emulateCpuThrottling?: boolean
-  cpuThrottlingRate?: number
+  emulateNetworkConditions?: string
+  emulateCpuThrottling?: number
 
   headless?: boolean
   timeout?: number
