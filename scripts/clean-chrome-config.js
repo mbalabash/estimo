2b8ae4d6282ef4a56bc999aa145da244c7434a55
Maksim Balabash <maksim.balabash@gmail.com>
2021-12-09 22:42:10 +0400

    Fix clean-chrome-config

--- scripts/clean-chrome-config.js
+++ scripts/clean-chrome-config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const { writeFile } = require('../src/utils')
 
 async function cleanChromeConfig() {
-  await writeFile(path.join(__dirname, '..', 'chrome.json'), '{ "executablePath": "" }')
+  await writeFile(path.join(__dirname, '..', 'chrome.json'), '{ "executablePath": "", "browser": "" }')
 }
 
 cleanChromeConfig()
