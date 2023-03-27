b0bcf0b0726be4e757b3838eb0be1c2d61735263
Maksim Balabash <maksim.balabash@gmail.com>
2023-03-27 05:59:31 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -221,7 +221,7 @@ export async function findChromeBinary() {
       return configData
     }
 
-    console.log(typeof puppeteer.BrowserFetcher, puppeteer.BrowserFetcher, puppeteer.PuppeteerNode.executablePath(), Object.keys(puppeteer))
+    console.log(typeof puppeteer.BrowserFetcher, puppeteer.BrowserFetcher, Object.keys(puppeteer))
     
     let chromeInfo = await findChrome({
       download: {
